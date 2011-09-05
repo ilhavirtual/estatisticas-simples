ebe0913c231f7e3247a99d934f929d10d6b3eb85
Felipe Munhoz <felipe@felipemunhoz.com>
2011-09-04 22:21:17 -0300

    Criados os testes e a API para os cálculos.

---
+++ src/EstatisticasSimples.java
@@ -0,0 +1,23 @@
+
+public class EstatisticasSimples {
+
+	public static double calculaValorMinimo(double[] valoresDeEntrada) {
+		return 0;
+	}
+
+	public static double calculaValorMaximo(double[] valoresDeEntrada) {
+		return 0;
+	}
+
+	public static int calculaQuantidade(double[] valoresDeEntrada) {
+		return 0;
+	}
+
+	public static double calculaMedia(double[] valoresDeEntrada) {
+		return 0;
+	}
+
+	public static double[] calcula(double[] valoresDeEntrada) {
+		return null;
+	}	
+}
